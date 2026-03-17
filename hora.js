66960aa7411069e7b6861a4264145cd473b2fdb2
Iarinha864 <iara.vitoria.ferreira@escola.pr.gov.br>
2026-03-17 14:56:56 +0000

    versao2

--- hora.js
+++ hora.js
@@ -1,3 +1,10 @@
 //Valores iniciais do dia
 let  valorPorhora = 50;
 let horasTrabalhadas = 8;
+
+console.log("--- Resumo do Expediente---");
+console.log(`Horas Iniciais:${horasTrabalhadas}h.`);
+
+//git add hora.js
+//git commit -m "versao2"
+//git push
