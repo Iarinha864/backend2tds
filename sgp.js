758f5f37912262f5a9140f0f815013c021b8acd6
Iarinha864 <iara.vitoria.ferreira@escola.pr.gov.br>
2026-04-29 13:53:06 +0000

    versao2

---
+++ sgp.js
@@ -0,0 +1,33 @@
+// sistema de gerenciamento de personagem
+
+function calcularPontuacaoTotal(historicoDePontos) {
+    const somarRodada = function(acumulador, pontosDaFase) {
+        const novaSoma = acumulador + pontosDaFase;
+        return novaSoma;
+    };
+    
+    const pontuacaoFinal = historicoDePontosDePontos.reduce(somarRodade, 0);
+    return pontuacaoFinal;
+};
+
+function filtrarMissoesConcluidas(listaDeMissoes) {
+    const verificarStatusDaMissao = function(missao) {
+        const foiFinalizada = missao.status === "concluída"; //=== Igualdade ESTRITA
+        return foiFinalizada;
+    };
+
+    const apenasConcluidas = listasDeMissoes.filter(verificarStatusDaMissao);
+    return apenasConcluidas
+};
+
+function atualizarInventario(inventarioAtual) {
+    let novoInventario;
+    if(acao === "pegar"){
+        const inventarioComItemNovo = [inventarioAtual, NomeDoItem];
+        novoInventario = inventarioComItemNovo;
+
+    }else if (acao === descartar) // parei aqui
+}
+
+const pontosDoJogador =[100,50,200,10]
+console.log(pontosDoJogador);
