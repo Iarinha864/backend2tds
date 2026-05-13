ae485bbfbdca48a1a43c57c3b18183d1c7c0454d
Iarinha864 <iara.vitoria.ferreira@escola.pr.gov.br>
2026-05-13 13:54:26 +0000

    versao1

---
+++ desafios.js
@@ -0,0 +1,15 @@
+//1-Declarar 5 variáveis para respresentar um funcionário 
+
+let nome = "Jasmine"
+let funcao = "Recpcionista"
+let empresa = "Psychology"
+let horario = "08:00 ás 14:00"
+const salario = 1500; 
+
+console.log(`O funcionário se chama ${nome} e a sua função no trabalho é ${funcao}, ele trabalha em uma empresa chamada ${empresa} e recebe um salário de ${salario}. Com um horário de trabalho de ${horario}`)
+
+
+//2- Crie duas variáveis númericas e exiba uma soma ou multiplicação 
+
+let salario = 3450; 
+let bonus = 200
