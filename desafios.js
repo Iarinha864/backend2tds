d200e3a7ebdbef67b256bc45f9802a8856dca7b5
Iarinha864 <iara.vitoria.ferreira@escola.pr.gov.br>
2026-05-18 13:54:40 +0000

    v2

--- desafios.js
+++ desafios.js
@@ -4,12 +4,20 @@ let nome = "Jasmine"
 let funcao = "Recpcionista"
 let empresa = "Psychology"
 let horario = "08:00 ás 14:00"
-const salario = 1500; 
+let salario = 1500; 
 
 console.log(`O funcionário se chama ${nome} e a sua função no trabalho é ${funcao}, ele trabalha em uma empresa chamada ${empresa} e recebe um salário de ${salario}. Com um horário de trabalho de ${horario}`)
 
 
 //2- Crie duas variáveis númericas e exiba uma soma ou multiplicação 
 
-let salario = 3450; 
-let bonus = 200
+let bonus = 200;
+let total =  salario + bonus;
+console.log (`Jasmine recebe um bonus quando cumpre com as descrições necessárias na sua rotina de trabalho, e no final do mês seu saldo total é R$ ${total} reais`)
+
+// 3- Declarar salário base e bônus. Calcular salário total e desconto de 10%
+
+let desconto_porcentagem = 10 
+
+ preco_final = total * (1- desconto_porcentagem/100)
+ console.log(preco_final)
